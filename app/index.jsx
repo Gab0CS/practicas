ffed7c88d7320d171acb64b9d2f4e113f0a5d048
Gab0CS <gabocanseco@gmail.com>
2024-06-26 00:21:31 -0600

    native wind terminado

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Sexo la pelicula</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile">Go to profile</Link>
+    </View>
+  );
+}
+
